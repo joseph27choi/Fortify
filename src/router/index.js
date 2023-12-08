445c188b40d89b96cf6236c21e6714549139662f
Joseph Choi <choi.joseph0302@gmail.com>
2023-12-07 23:47:19 -0500

    created separate web-nodejs folder

--- src/router/index.js
+++
@@ -1,8 +0,0 @@
-const Router = require("express")
-// import user
-const USERROUTE = require("./user/index")
-
-const indexRouter = Router();
-indexRouter.use('/user', USERROUTE)
-
-module.exports = indexRouter;
